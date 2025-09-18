20d97af1793f8e78839a1642c10d61ca03069fa9
Mario Lazzari <mario.lazzari@gmail.com>
2025-09-18 08:04:24 +0200

    Done lesson 1

---
+++ 1_wrapping/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result string
+	for i := range 5 {
+		err := isOdd(i)
+		if err != nil {
+			result += fmt.Sprintf("Error: %v\n", err)
+		} else {
+			result += "Number is odd\n"
+		}
+		// fmt.Println(result)
+	}
+}
+
+func isOdd(n int) error {
+	if n%2 == 0 {
+		return fmt.Errorf("number %d is even", n)
+	}
+	return nil
+}
